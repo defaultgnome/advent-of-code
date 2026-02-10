5071caae689895403ca5855855ef0ce6d23d3532
defaultgnome <defaultgnome@users.noreply.github.com>
2026-02-10 11:43:19 +0200

    feat(c): unity build + header only lib

--- c/2025/01/main.c
+++ c/2025/01/main.c
@@ -1,8 +1,9 @@
-#include "utils.h"
+#define DFGN_UTILS_IMPLEMENTAION
+#include "../../intern/dfgn_utils.h"
 #include <stdio.h>
 
 int main() {
-  int result = add(20, 24);
+  int result = dfgn_utils_add(1, 2);
   printf("this is the result %d\n", result);
 
   return 0;
---
+++ c/intern/dfgn_utils.h
@@ -0,0 +1,13 @@
+
+#ifndef INCLUDE_DFGN_UTILS_H
+#define INCLUDE_DFGN_UTILS_H
+
+int dfgn_utils_add(int a, int b);
+
+#endif // INCLUDE_DFGN_UTILS_H
+
+#ifdef DFGN_UTILS_IMPLEMENTAION
+// API
+int dfgn_utils_add(int a, int b) { return a + b; }
+
+#endif // DFGN_UTILS_IMPLEMENTAION
